ec0afb4dacb2317d54ff05f92b189af44b678428
t-coffe <tcoffe@edenschool.fr>
2023-11-08 16:13:33 +0100

    menuBurger

---
+++ main.js
@@ -0,0 +1,10 @@
+let barreHaut = document.querySelector("#barre-haut");
+let barreMilieu = document.querySelector("#barre-milieu");
+let barreBas = document.querySelector("#barre-bas");
+let menuBurger = document.querySelector("#menu-burger");
+let p = document.querySelector("p");
+let reste = document.querySelector("#texte");
+
+p.addEventListener("click", ()=>{
+  reste.displayNone = false;
+});
